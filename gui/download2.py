3d50d4ad5e9cc829afd4aea107615342280bb673
itsHenry35 <2671230065@qq.com>
2022-08-24 18:00:17 +0800

    Big update~ No console shown!!! Gui for downloading page!!!

--- gui/download2.py
+++ gui/download2.py
@@ -1,6 +1,12 @@
 # import the things we need
+from tkinter.ttk import Progressbar
 import requests
 import os
+import subprocess
+from pyaria2 import Aria2RPC
+import ttkbootstrap as ttk
+import time
+import threading
 
 def getlecturers(list, uid, token):
     url = "https://course-api-online.saasp.vdyoo.com/course/v1/student/course/user-live-list" #all the lecturers
@@ -62,23 +68,64 @@ def getdownloadurl(id, uid, token):
     videourl = json['videoUrls'][2] # get the download url of mp4 from the info
     return videourl
 
-
-def aria2_download(filename, url):
-    path = os.path.join('downloaded', name) #create a folder to store the downloaded files
-    if not os.path.exists(path):
-        os.makedirs(path) 
-    arguments = ' -d ' + path + ' -j 64 --file-allocation=none ' + ' -o ' + filename # dir is downloads and the name, 64 thread fast downloading and filename should be changed as it is given when using aria2_download
-    run = aria2c_path + ' ' + url + arguments # finally generate the command of aria2
-    os.system(run) # run it!
     
+
 def download2(list, uid, token, path__):
-    global aria2c_path
-    aria2c_path = path__ #get which aria2c binary to use
+    gid_group = {}
+    def aria2_download(link, path, filename):
+        options = {"dir": path, "out": filename, }
+        download_ = jsonrpc.addUri([link], options = options)
+        gid_group[filename] = download_
+    def get_stat():
+        while True:
+            for filename in gid_group:
+                stat = jsonrpc.tellStatus(gid = str(gid_group[filename]))
+                if stat['status'] != 'complete':
+                    stat = jsonrpc.tellStatus(str(gid_group[filename]))
+                    if int(stat['totalLength']) != 0 and int(stat['completedLength']) != 0:
+                        tkinterlist[filename]['progress'].value = int(stat['completedLength']) / int(stat['totalLength']) * 100
+                        tkinterlist[filename]['progress'].update()
+                        tkinterlist[filename]['percentage'].configure(text=str("%.2f"%((int(stat['completedLength']) / int(stat['totalLength']) * 100))) + '%')
+                        tkinterlist[filename]['speed'].configure(text='下载速度：' + round(str(int(stat['downloadSpeed'])/1024/1024), 2) + 'MB/s')
+            time.sleep(0.1)
+    root = ttk.Window(title = '乐读视频下载器-下载', themename="morph")
+    root.geometry('1280x720')
+    aria2process = subprocess.Popen(path__ + ' --enable-rpc --rpc-listen-port=6800 --max-connection-per-server=16 --file-allocation=none -j=64',shell=True)
+    jsonrpc = Aria2RPC()
     id = getlecturers(list, uid, token)
     downloadurls = {}
     for i in id:
         url = getdownloadurl(str(id[i]), uid, token)
         filename = '第'+ str(i) + '讲.mp4'
         downloadurls[filename] = url
+    count = 2
+    tkinterlist = {}
+    text = ttk.Label(text = '下载中~')
+    text.grid(row=1, column=0)
     for i in downloadurls:
-        aria2_download(i, downloadurls[i])
+        text = ttk.Label(text = i)
+        text.grid(row=count, column=0)
+        progress = ttk.Progressbar(bootstyle="striped")
+        progress.grid(row=count, column=1)
+        progress.maximum = 100
+        progress.value = 0
+        text0 = ttk.Label(text = '0%')
+        text0.grid(row=count, column=1)
+        text1 = ttk.Label(text = '下载速度：Nah')
+        text1.grid(row=count, column=2)
+        tkinterlist[i] = {
+            'progress': progress,
+            'percentage': text0,
+            'speed': text1,
+        }
+        count += 1
+    path = os.path.join('乐读-下载', name) #create a folder to store the downloaded files
+    if not os.path.exists(path):
+        os.makedirs(path) 
+    for i in downloadurls:
+         aria2_download(downloadurls[i], path, i)  
+    thread = threading.Thread(target=get_stat)
+    thread.setDaemon(True)
+    thread.start()
+    root.mainloop()
+    return
