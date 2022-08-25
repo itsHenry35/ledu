6aaee41bc09e074bae16c0c3ec07107b41e719f5
itsHenry35 <2671230065@qq.com>
2022-08-25 12:33:23 +0800

    SUPPORT_SMS!!!

--- gui/login1.py
+++ gui/login1.py
@@ -3,14 +3,25 @@ from ttkbootstrap.constants import *
 import importlib
 
 def login1():
+    global sms
+    global root
+    global username
     def switchshowpwd():
         if password['show'] == '':
             password['show'] = '*'
         else:
             password["show"] = ''
+    def switchtosms():
+        global username_
+        global sms
+        sms = 'True'
+        username_ = username.get()
+        root.destroy()
     def nextpage():
         global username_
         global password_
+        global sms
+        sms = 'False'
         username_ = username.get()
         password_ = password.get()
         root.destroy()
@@ -26,12 +37,21 @@ def login1():
     text2.grid(row=2)
     password = ttk.Entry(bootstyle="primary", show="*")
     password.grid(row=2,column=1)
-    showpwd = ttk.Button(text='显示/隐藏密码', bootstyle="default-outline", command=switchshowpwd)
+    showpwd = ttk.Button(text='显示/隐藏密码', bootstyle="defaulte", command=switchshowpwd)
     showpwd.grid(row=2,column=2)
+    smsswitch = ttk.Button(text='短信验证码登陆', bootstyle="default-outline", command=switchtosms)
+    smsswitch.grid(row=2,column=3)
     submit = ttk.Button(text='提交', bootstyle="primary", command=nextpage)
-    submit.grid(row=3, column=1)
+    submit.grid(row=3, column=2)
     root.mainloop()
     importlib.reload(ttk.style)
-    return {'usrname':username_, 
-        'pwd':password_,
+    if sms == 'False':
+        return {'success' : 'True',
+            'usrname':username_, 
+            'pwd':password_,
+            }
+    if sms == 'True':
+        return {
+            'success' : 'False',
+            'phonenum' : username_
         }
