70ba0b1bc98156c26094c356515c23d9513b73c1
itsHenry <2671230065@qq.com>
2023-06-28 15:45:26 +0800

    feat: support multi course downloading and custom download path

--- gui/download1.py
+++ gui/download1.py
@@ -2,6 +2,8 @@ import requests
 import ttkbootstrap as ttk
 from ttkbootstrap.constants import *
 import importlib
+from tkinter.filedialog import askdirectory
+import tkinter.messagebox as mb
 
 def get_course(uid, token):
     url = "https://course-api-online.saasp.vdyoo.com/course/v1/student/course/list"
@@ -22,36 +24,53 @@ def get_course(uid, token):
     response = requests.get(url, headers=headers)
     return response.json()
 
+def select_path():
+    global path
+    path = askdirectory()
+
 def download1(uid, token):
-    def select_course():
-        selected_course = numlist[check_box_var1.get()]
-        returnlist['tutorid'] = courselist[selected_course]
-        returnlist['courseid'] = idlist[selected_course]
-        returnlist['name'] = selected_course
-        root.destroy()
-        returnlist['extensiveornot'] = 'True' if var.get() == 1 else 'False'
+    global path
+    def submit():
+        count = 0
+        for i, widget in enumerate(widgetlist):
+            if 'selected' in widget.state():
+                returnlist.append({})
+                returnlist[count]['tutorid'] = courselist[numlist[i]]
+                returnlist[count]['courseid'] = idlist[numlist[i]]
+                returnlist[count]['name'] = numlist[i]
+                returnlist[count]['extensiveornot'] = 'True' if var.get() == 1 else 'False'
+                count+=1
+        if count == 0:
+            mb.showwarning(title='警告', message='未选择课程')
+        else:
+            root.destroy()
+
 
     root = ttk.Window(title='乐读视频下载器-下载', themename="morph")
     root.geometry("")
     data = get_course(uid, token)
-    courselist, idlist, numlist, returnlist = {}, {}, [], {}
+    courselist, idlist, numlist, returnlist = {}, {}, [], []
     var = ttk.IntVar()
     var.set(0)
+    path = ""
 
     for course in data:
         courselist[course['courseName']] = course['tutorId']
         idlist[course['courseName']] = course['stdCourseId']
 
-    check_box_var1 = ttk.IntVar()
-    for index, course_name in enumerate(courselist):
+    widgetlist = []
+    for course_name in courselist:
         numlist.append(course_name)
-        radiobutton = ttk.Radiobutton(text=course_name, bootstyle="primary-outline-toolbutton", variable=check_box_var1, value=index)
-        radiobutton.pack(anchor='w')
+        checkbutton = ttk.Checkbutton(text=course_name, bootstyle="round-toggle")
+        checkbutton.pack(anchor='w')
+        widgetlist.append(checkbutton)
 
-    extensiveornot = ttk.Checkbutton(text='延伸课程', bootstyle="default-round-toggle", variable=var)
+    extensiveornot = ttk.Checkbutton(text='延伸课程', bootstyle="default-square-toggle", variable=var)
     extensiveornot.pack(anchor='w')
-    submit = ttk.Button(text='提交', bootstyle="primary", command=select_course)
+    submit = ttk.Button(text='提交', bootstyle="primary", command=submit)
     submit.pack(anchor='w')
+    selectpath = ttk.Button(text='选择自定义路径（可不选，默认为程序所在目录）', bootstyle="primary-outline-toolbutton", command=select_path)
+    selectpath.pack(anchor='w')
     root.mainloop()
     importlib.reload(ttk.style)
-    return returnlist
+    return returnlist, path
